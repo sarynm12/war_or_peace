4aecc9208b754b2a295f2675c5840d3f04fe5a7b
Saryn Mooney <smooney15@my.whitworth.edu>
2020-06-23 20:53:59 -0600

    card class created

--- lib/card.rb
+++ lib/card.rb
@@ -1,4 +1,13 @@
 class Card
   attr_reader :suit, :value, :rank
 
-end 
+  def initialize(suit, value, rank)
+    @suit = suit
+    @value = value
+    @rank = rank
+  end
+end
+
+@suit = %w{Spades Hearts Diamonds Clubs}
+@value = %w{A 2 3 4 5 6 7 8 9 10 J Q K}
+@rank = 1..12
