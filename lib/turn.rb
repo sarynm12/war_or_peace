ee5ae762a1b58219456651368d7ba4a553f5be51
Saryn Mooney <smooney15@my.whitworth.edu>
2020-06-29 19:31:38 -0600

    first part of pile cards method written

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,10 +10,31 @@ class Turn
   def type
     if player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0) && player1.deck.rank_of_card_at(2) == player2.deck.rank_of_card_at(2)
       :mutually_assured_destruction
-    elsif player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
-      :basic
     elsif player1.deck.rank_of_card_at(0) == player2.deck.rank_of_card_at(0)
       :war
+    elsif player1.deck.rank_of_card_at(0) != player2.deck.rank_of_card_at(0)
+      :basic
+    end
+  end
+
+  def winner
+    if type == :basic && player1.deck.rank_of_card_at(0) > player2.deck.rank_of_card_at(0)
+      player1
+    elsif type == :basic && player2.deck.rank_of_card_at(0) > player1.deck.rank_of_card_at(0)
+      player2
+    elsif type == :war && player1.deck.rank_of_card_at(2) > player2.deck.rank_of_card_at(2)
+      player1
+    elsif type == :war && player2.deck.rank_of_card_at(2) > player1.deck.rank_of_card_at(2)
+      player2
+    elsif type == :mutually_assured_destruction
+      "No Winner"
+    end
+  end
+
+  def pile_cards
+    if type == :basic
+      @spoils_of_war << player1.deck.remove_card
+      @spoils_of_war << player2.deck.remove_card
     end
   end
 
--- test/turn_test.rb
+++ test/turn_test.rb
@@ -77,54 +77,65 @@ class TurnTest < Minitest::Test
     assert_equal :basic, turn.type
   end
 
+  def test_it_can_return_winner
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_equal player1, turn.winner
+  end
+
+  def test_it_can_return_another_winner_when_decks_are_switched
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player2 = Player.new("Megan", deck1)
+    player1 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    assert_equal player2, turn.winner
+  end
+
+  def test_pile_cards
+    card1 = Card.new(:heart, 'Jack', 11)
+    card2 = Card.new(:heart, '10', 10)
+    card3 = Card.new(:heart, '9', 9)
+    card4 = Card.new(:diamond, 'Jack', 11)
+    card5 = Card.new(:heart, '8', 8)
+    card6 = Card.new(:diamond, 'Queen', 12)
+    card7 = Card.new(:heart, '3', 3)
+    card8 = Card.new(:diamond, '2', 2)
+    deck1 = Deck.new([card1, card2, card5, card8])
+    deck2 = Deck.new([card3, card4, card6, card7])
+    player1 = Player.new("Megan", deck1)
+    player2 = Player.new("Aurora", deck2)
+    turn = Turn.new(player1, player2)
+    turn.pile_cards
+    assert_equal [card1, card3], turn.spoils_of_war
+    assert_equal 2, turn.spoils_of_war.count 
+    assert_equal [card2, card5, card8], player1.deck.cards
+    assert_equal [card4, card6, card7], player2.deck.cards
+  end
+
 end
 
 # The Turn is responsible for much of the logic of the game and will respond to the following methods:
 
-# pry(main)> card1 = Card.new(:heart, 'Jack', 11)
-# #=> #<Card:0x007fa3edaa0df0 @rank=11, @suit=:heart, @value="Jack">
-# pry(main)> card2 = Card.new(:heart, '10', 10)
-# #=> #<Card:0x007fa3eda519a8 @rank=10, @suit=:heart, @value="10">
-# pry(main)> card3 = Card.new(:heart, '9', 9)
-# #=> #<Card:0x007fa3ed98d9b8 @rank=9, @suit=:heart, @value="9">
-# pry(main)> card4 = Card.new(:diamond, 'Jack', 11)
-# #=> #<Card:0x007fa3ee14ef80 @rank=11, @suit=:diamond, @value="Jack">
-# pry(main)> card5 = Card.new(:heart, '8', 8)
-# #=> #<Card:0x007fa3edb263d8 @rank=8, @suit=:heart, @value="8">
-# pry(main)> card6 = Card.new(:diamond, 'Queen', 12)
-# #=> #<Card:0x007fa3eda3e1f0 @rank=12, @suit=:diamond, @value="Queen">
-# pry(main)> card7 = Card.new(:heart, '3', 3)
-# #=> #<Card:0x007fa3edad1cc0 @rank=3, @suit=:heart, @value="3">
-# pry(main)> card8 = Card.new(:diamond, '2', 2)
-# #=> #<Card:0x007fa3eda89308 @rank=2, @suit=:diamond, @value="2">
-#
-# pry(main)> deck1 = Deck.new([card1, card2, card5, card8])
-# #=> #<Deck:0x007fa3eda472c8 @cards=[#<Card:0x007fa3edaa0df0...>, #<Card:0x007fa3eda519a8...>, #<Card:0x007fa3edb263d8...>, #<Card:0x007fa3eda89308...>]>
-#
-# pry(main)> deck2 = Deck.new([card3, card4, card6, card7])
-# #=> #<Deck:0x007fa3ee11ee48 @cards=[#<Card:0x007fa3ed98d9b8...>, #<Card:0x007fa3ee14ef80...>, #<Card:0x007fa3eda3e1f0...>, #<Card:0x007fa3edad1cc0...>]>
-#
-# pry(main)> player1 = Player.new("Megan", deck1)
-# #=> #<Player:0x007fa3edae29d0 @deck=#<Deck:0x007fa3eda472c8...>, @name="Megan">
-#
-# pry(main)> player2 = Player.new("Aurora", deck2)
-# #=> #<Player:0x007fa3ed9e6568 @deck=#<Deck:0x007fa3ee11ee48...>, @name="Aurora">
-#
-# pry(main)> turn = Turn.new(player1, player2)
-# #=> #<Turn:0x007fa3edb25d20 @player1=#<Player:0x007fa3edae29d0..., @name="Megan">, @player2=#<Player:0x007fa3ed9e6568..., @name="Aurora">, @spoils_of_war=[]>
-#
-# pry(main)> turn.player1
-# #=> #<Player:0x007fa3edae29d0 @deck=#<Deck:0x007fa3eda472c8...>, @name="Megan">
-#
-# pry(main)> turn.player2
-# #=> #<Player:0x007fa3ed9e6568 @deck=#<Deck:0x007fa3ee11ee48...>, @name="Aurora">
-#
-# pry(main)> turn.spoils_of_war
-# #=> []
-#
-# pry(main)> turn.type
-# #=> :basic
-#
 # pry(main)> winner = turn.winner
 # #=> #<Player:0x007fa3edae29d0 @deck=#<Deck:0x007fa3eda472c8...>, @name="Megan">
 #
